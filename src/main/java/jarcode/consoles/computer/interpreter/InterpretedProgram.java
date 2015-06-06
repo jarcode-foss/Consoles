650d54d60e79b70446f99c2463162d3e6f3b7ea4
BuildTools <wacossusca34@gmail.com>
2015-06-06 02:02:57 -0700

    finally documented most of InterpretedProgram

--- src/main/java/jarcode/consoles/computer/interpreter/InterpretedProgram.java
+++ src/main/java/jarcode/consoles/computer/interpreter/InterpretedProgram.java
@@ -75,6 +75,8 @@ public class InterpretedProgram {
 		// maps all functions that start with lua$
 		Lua.find(this, pool);
 
+		// configurable API options
+
 		if (Consoles.componentRenderingEnabled) {
 			Lua.put(this::lua_registerPainter, "paint", pool);
 		}
@@ -92,12 +94,23 @@ public class InterpretedProgram {
 			this.computer = computer;
 			this.instance = instance;
 			this.args = str;
-			pool = new FuncPool(Thread.currentThread(), this);
-			map();
+
+			// if the file is null, something went wrong
 			if (file == null) {
 				print("null file");
 				return;
 			}
+
+			// this is our function pool, which are a bunch of LuaFunctions mapped to strings
+			// we also use this pool to identify our program with this thread.
+			//
+			// all static functions that were already mapped are automatically added to this pool
+			pool = new FuncPool(Thread.currentThread(), this);
+
+			// map functions from this program instance to the pool
+			map();
+
+			// read from the program file and write it to a buffer
 			ByteArrayOutputStream buf = new ByteArrayOutputStream();
 			try (InputStream is = file.createInput()) {
 				int i;
@@ -113,29 +126,52 @@ public class InterpretedProgram {
 				if (terminated())
 					print(" [PARSE TERMINATED]");
 			}
+
+			// parse as a string
 			String raw = new String(buf.toByteArray(), charset);
+
+			// gc buffer
+			buf = null;
+
+			// create our globals for Lua. We use a special kind of globals
+			// that allows us to finalize variables.
 			globals = new EmbeddedGlobals();
+
+			// Load libraries from LuaJ. I left a bunch of libraries from the
+			// JSE standards to have less possibilities for users to exploit
+			// them.
 			globals.load(new JseBaseLib());
 			globals.load(new PackageLib());
 			globals.load(new Bit32Lib());
 			globals.load(new TableLib());
 			globals.load(new StringLib());
+			globals.load(new BaseLib());
+
+			// I added a missing function to the math library
 			globals.load(new EmbeddedMathLib());
+
+			// Load our debugging library, which is used to terminate the program
 			globals.load(interruptLib);
+
+			// Load any extra libraries, these can be registered by other plugins
+			// Note, we only register libraries that are not restricted.
 			Lua.libraries.values().stream()
 					.filter((lib) -> !lib.isRestricted)
 					.forEach((lib) -> globals.load(lib.buildLibrary()));
+
+			// install
 			LoadState.install(globals);
 			LuaC.install(globals);
 
-			globals.load(new BaseLib());
-
+			// load functions from our pool
 			for (Map.Entry<String, LibFunction> entry : pool.functions.entrySet()) {
 				globals.set(entry.getKey(), entry.getValue());
 			}
 
+			// set stdout
 			globals.STDOUT = new PrintStream(out);
-			// we handle errors with exceptions
+
+			// we handle errors with exceptions, so this will be a dummy writer.
 			globals.STDERR = new PrintStream(out) {
 				@Override
 				public void println(String x) {}
@@ -143,13 +179,29 @@ public class InterpretedProgram {
 				@Override
 				public void println(Object x) {}
 			};
+
+			// set stdin
 			globals.STDIN = in;
+
+			// finalize all entries. This means programs cannot modify any created
+			// globals at this point.
 			globals.finalizeEntries();
+
+			// our main program chunk
 			LuaValue chunk;
+
+			// the exit function, if it exists.
 			LuaValue exit = null;
+
 			try {
+
+				// try to load in the program
+				// this will try to compile the Lua string into Java bytecode
 				chunk = globals.load(raw);
-			} catch (LuaError err) {
+
+			}
+			// if we run into a compile error, print out the details and exit.
+			catch (LuaError err) {
 				if (Consoles.debug)
 					err.printStackTrace();
 				println("lua:" + ChatColor.RED + " compile error");
@@ -159,50 +211,94 @@ public class InterpretedProgram {
 				print(msg);
 				return;
 			}
+			// if we got to this point, the program compiled just fine.
 			try {
+
+				// Call the main chunk. This will start executing the Lua program
+				// in this thread, and will return when the chunk has been completely
+				// executed.
 				chunk.call();
+
+				// After this point, we can call the main method since the chunk was
+				// just called, and all the methods in said chunk have been declared.
+
+				// get our main function
 				LuaValue value = globals.get("main");
+
+				// set the exit function
 				exit = globals.get("exit");
+
+				// if the main function exists, call it.
+				//
+				// some programs won't have a main method. That's fine, in that case
+				// most of the code will be in the chunk itself.
 				if (value.isfunction()) {
 					value.call(args);
 				}
 			}
+			// if the program was interrupted by our debug/interrupt lib
 			catch (ProgramInterruptException ex) {
 				print("\nProgram terminated");
 			}
+			// if we encountered an error, we go through quite the process to handle it
 			catch (LuaError err) {
 				handleLuaError(err);
 			}
+			// regardless if we encountered an error or not, we try to call our exit function.
 			finally {
+				// if the exit function exists, and our program has not been terminated
 				if (exit != null && exit.isfunction() && !terminated()) {
+
 					try {
+
+						// call the function
 						exit.call();
+
 					}
+					// again, if the exit function was interrupted.
 					catch (ProgramInterruptException ex) {
 						print("\nExit routine terminated");
 					}
+					// if there was an error, handle it the same way.
 					catch (LuaError err) {
 						handleLuaError(err);
 					}
 				}
 			}
 		}
+		// cleanup code
 		finally {
+
+			// unregister our pool
 			if (pool != null)
 				pool.cleanup();
+
+			// clear all frame references (from the Lua graphics API)
 			framePool.clear();
+
+			// remove all registered channels (from the Lua networking API)
 			registeredChannels.forEach(computer::unregisterMessageListener);
 			registeredChannels.clear();
+
+			// remove terminal hooks
 			Terminal terminal = computer.getTerminal(this);
 			if (terminal != null) {
+				// user input handling
 				terminal.setHandlerInterrupt(null);
+				// hook to remove termination from users other than the owner
 				terminal.setIgnoreUnauthorizedSigterm(false);
 			}
+
+			// remove all components that were set to any screen session(s)
 			for (int i : allocatedSessions) {
 				computer.setComponent(i, null);
 			}
 		}
+
+		// at the end of this method, in one way or another, our Lua program will have ended.
 	}
+
+	// finds a new id for a frame that is not taken
 	private int findFrameId() {
 		int i = 0;
 		while(framePool.containsKey(i))
@@ -213,15 +309,22 @@ public class InterpretedProgram {
 		return computer;
 	}
 	private void handleLuaError(LuaError err) {
+
+		// print stack trace in console if in debug mode
 		if (Consoles.debug)
 			Consoles.getInstance().getLogger().severe("\n" + ExceptionUtils.getFullStackTrace(err));
+
+		// start of our error string
 		String errorBreakdown = err.getMessage();
-		boolean inst;
+
+		// We check for errors that caused the top-level error we just encountered, and add their
+		// information to the breakdown.
+		boolean cont;
 		do {
-			inst = false;
+			cont = false;
 			if (err.getCause() instanceof LuaError) {
 				errorBreakdown += "\n\nCaused by:\n" + err.getCause().getMessage();
-				inst = true;
+				cont = true;
 			}
 			else if (err.getCause() instanceof InvocationTargetException) {
 				if (((InvocationTargetException) err.getCause()).getTargetException() != null)
@@ -230,19 +333,31 @@ public class InterpretedProgram {
 							+ ": " + ((InvocationTargetException) err.getCause()).getTargetException().getMessage();
 				if (((InvocationTargetException) err.getCause()).getTargetException() instanceof LuaError) {
 					err = (LuaError) ((InvocationTargetException) err.getCause()).getTargetException();
-					inst = true;
+					cont = true;
 				}
 			}
 		}
-		while (inst);
+		while (cont);
+
+		// tell the user we encountered a runtime error
 		println("lua:" + ChatColor.RED + " runtime error");
+
+		// split into lines and color each line
 		String[] arr = Arrays.asList(errorBreakdown.split("\n")).stream()
 				.map(this::err)
 				.toArray(String[]::new);
+		// combine again
 		String msg = Joiner.on('\n').join(arr);
+
+		// if the amount of lines in the breakdown is greater than 16, dump it to a file
 		if (arr.length > 16) {
+
+			// get terminal instance
 			Terminal terminal = computer.getTerminal(this);
 			if (terminal != null) {
+
+				// find file name to use. We use LuaFile because
+				// it's a lot easier than using FSFile.
 				LuaFile file;
 				int i = 0;
 				while (resolve("lua_dump" + i) != null) {
@@ -250,6 +365,8 @@ public class InterpretedProgram {
 				}
 				file = lua$touch("lua_dump" + i);
 				assert file != null;
+
+				// grab Lua version
 				String version;
 				try {
 					version = globals.get("_VERSION").checkjstring();
@@ -257,10 +374,16 @@ public class InterpretedProgram {
 				catch (LuaError ignored) {
 					version = "?";
 				}
+
+				// prefix and transform message
 				msg = "Lua stack trace from " + format.format(new Date(System.currentTimeMillis())) + "\n"
 						+ "Lua version: " + version + "\n\n"
 						+ ChatColor.stripColor(msg.replace("\t", "    "));
+
+				// write the data to the file
 				file.write(msg);
+
+				// tell the user we dumped the error breakdown to a file
 				String cd = terminal.getCurrentDirectory();
 				if (cd.endsWith("/"))
 					cd = cd.substring(0, cd.length() - 1);
@@ -268,6 +391,7 @@ public class InterpretedProgram {
 				print("lua:" + ChatColor.RED + " dumped: " + ChatColor.YELLOW + cd + "/" + "lua_dump" + i);
 			}
 		}
+		// if small enough, print the error directly in the terminal
 		else
 			print(msg);
 	}
@@ -277,6 +401,31 @@ public class InterpretedProgram {
 	private String err(String str) {
 		return "\t" + ChatColor.RED + str;
 	}
+	protected void print(String formatted) {
+		try {
+			out.write(formatted.getBytes(charset));
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	protected void println(String formatted) {
+		print(formatted + '\n');
+	}
+	protected void nextL() {
+		print("\n");
+	}
+	protected FSBlock resolve(String input) {
+		return computer.resolve(input, this);
+	}
+	protected boolean terminated() {
+		return instance.isTerminated();
+	}
+
+	//
+	// Below are all functions visible to the Lua program (identified by the 'lua$' prefix)
+	//
+
 	public LuaTypeBuilder lua$typeBuilder() {
 		return new LuaTypeBuilder();
 	}
@@ -527,25 +676,5 @@ public class InterpretedProgram {
 	private void lua$printc(String formatted) {
 		formatted = ChatColor.translateAlternateColorCodes('&', formatted);
 		println(formatted);
-	}
-	protected void print(String formatted) {
-		try {
-			out.write(formatted.getBytes(charset));
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	protected void println(String formatted) {
-		print(formatted + '\n');
-	}
-	protected void nextL() {
-		print("\n");
-	}
-	protected FSBlock resolve(String input) {
-		return computer.resolve(input, this);
-	}
-	protected boolean terminated() {
-		return instance.isTerminated();
 	}
 }
