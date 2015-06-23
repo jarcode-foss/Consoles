8d83141587300317ad8f680034edd736dd2c5e82
Levi Webb <wacossusca34@gmail.com>
2015-06-23 09:18:59 -0700

    Update build.sh
    Build script no longer packages artifacts into a zip because:
    
    - There is an online repository
    - The build script was broken and it deleted all the files in my home folder on my computer.

--- build.sh
+++ build.sh
@@ -2,20 +2,5 @@
 
 # This script is used to build and package Consoles locally
 
-rm target/Consoles.zip
-cd target
-for FILENAME in *.jar; do rm -f ${FILENAME}; done
-cd bukkit-final
-for FILENAME in *.jar; do rm -f ${FILENAME}; done
-cd ../../bungee/target/
-for FILENAME in *.jar; do rm -f ${FILENAME}; done
-cd ../..
 mvn package -Dmaven.test.skip=true | egrep -v "(^\[WARNING\])|(already added\, skipping)"
 mvn package -pl bungee -am -Dmaven.test.skip=true | egrep -v "(^\[WARNING\])|(already added\, skipping)"
-cd target/final/
-for FILENAME in *; do mv ${FILENAME} consoles.jar; done
-zip ../Consoles.zip consoles.jar
-cd ../../bungee/target/
-for FILENAME in *.jar; do mv ${FILENAME} bungee-consoles.jar; done
-zip ../../target/Consoles.zip bungee-consoles.jar
-cd ../..
