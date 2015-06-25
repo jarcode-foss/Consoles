246d515a325a13e6a633366dec5de0c4ab1570e0
BuildTools <wacossusca34@gmail.com>
2015-06-24 18:46:50 -0700

    resource packaging

--- craftbukkit/src/main/java/ca/jarcode/consoles/Lang.java
+++ craftbukkit/src/main/java/ca/jarcode/consoles/Lang.java
@@ -1,7 +1,18 @@
 package ca.jarcode.consoles;
 
 import java.util.ResourceBundle;
+import java.util.logging.Logger;
 
 public class Lang {
-	public static final ResourceBundle lang = ResourceBundle.getBundle("ca.jarcode.consoles");
+	public static final ResourceBundle lang;
+	static {
+		try {
+			lang = ResourceBundle.getBundle("ca.jarcode.consoles");
+		}
+		catch (Throwable e) {
+			Logger.getGlobal().severe("Failed to load resource bundle: ");
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
 }
