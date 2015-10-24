9e6d49951c8e71eebb571f02593c9bad006e6934
BuildTools <wacossusca34@gmail.com>
2015-10-24 16:12:08 -0700

    build script changes

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -e
 mvn compile
-mkdir consoles-computers/target/natives
+mkdir -p consoles-computers/target/natives
 cd consoles-computers
+rm -f target/natives/libcomputerimpl.so
 make all
 cd ..
 mvn install
